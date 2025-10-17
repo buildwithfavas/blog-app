bd8e725ad1ec9ccd86457352e4dd36b79cff9146
Favas <72879643+buildwithfavas@users.noreply.github.com>
2025-10-17 18:59:40 +0530

    Changes done for base path in production in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  base: import.meta.env.VITE_BASE_PATH || "/blog-app",
+export default defineConfig(({ mode }) => {
+  // Load env file based on current mode (development / production)
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
+    base: env.VITE_BASE_PATH || "/blog-app",
+  }
 });
